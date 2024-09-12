7c4772d5e91977b094f8c8fa38fe929f17a8b417
Art38169 <00482@kvis.ac.th>
2024-09-12 20:13:32 +0700

    day4.py

--- day3.py
+++ day3.py
@@ -1,10 +1,10 @@
-"""name = "Vetit so handsome"
+name = "Vetit so handsome"
 age = 42
 # reserved variables are "False", "int", ... See by using the function help("keywords")
 name_input = input("Your name is ")
 age_input = int(input("How old are you "))
 print("Hello", name_input, "You are", age_input, "years old.")
-"""
+
 """a = 4 + 4
 a = a + 4
 b = 3 ** 2
