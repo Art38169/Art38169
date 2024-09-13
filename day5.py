06f8fd563569e7bea49b3bc0577b209132a7fb4e
Art38169 <00482@kvis.ac.th>
2024-09-13 19:30:14 +0700

    day4.py

---
+++ day5.py
@@ -0,0 +1,20 @@
+print("Vetit the singer is tired af.")
+# if, else, elif
+pocket_money = float(input("How much money you have boi? "))
+age = int(input("Enter your age"))
+if pocket_money > 3000:
+    print("cavier and foie gras")
+    print("Johnnie Walker")
+elif pocket_money > 1000:
+    print("Fancy Buffet")
+    print("Wine")
+elif pocket_money > 500:
+    print("Normal Buffet")
+elif pocket_money > 200:
+    print("Tomyum", "Kaprao")
+    print("Fried eggs")
+else:
+    print("Fuck you")
+
+
+    
