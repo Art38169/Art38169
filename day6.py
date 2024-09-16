4b4f2a04181103436c08a75d265578deaf3520fc
Art38169 <00482@kvis.ac.th>
2024-09-16 20:01:54 +0700

    day6.py

---
+++ day6.py
@@ -0,0 +1,35 @@
+print("Hello, Mr.Vetit Kanjaras")
+# While Loops 
+"""
+while CONDITION:
+    ACTION
+    
+Example
+
+num = 1
+while num <= 10:
+    print(2 * num)
+    num += 1
+"""
+# Break, Coninue, Pass
+"""num = 1
+while True:
+    print(num)
+    if num % 3 == 0 and num % 2 == 0:
+        break
+    num += 1
+"""
+# For loop
+# range(start, stop, step)
+# range(6) -> 0,1,2,3,4,5 ; 6 is a "stop" | range(-2) -> Nothing
+# range(2, 6) -> 2,3,4,5 ; 2 is a "start" | range(9, 3) -> Nothing
+# range(3, 9, 2) -> 3,5,7 ; 2 is a "step" | range(3, 0, -1) -> 3,2,1
+"""
+for variable_name in range(SOMETHING):
+    DO_SOMETHING
+
+for number in range(1, 101):
+    square = number ** 2
+    print(number, square)
+"""
+
