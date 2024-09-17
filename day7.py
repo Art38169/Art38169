08db8b16ea31107aa653b6c9b5dc44c16021588e
Art38169 <00482@kvis.ac.th>
2024-09-17 19:34:59 +0700

    day7.py

---
+++ day7.py
@@ -0,0 +1,21 @@
+print("The fuck is that project?!?!")
+#Nested Loop
+"""for i in range(5):
+    for j in range(1, i):
+        print(i * j)
+"""
+#String
+# quote = "She said that \"I'm a boy.\""   \" is to escape the character
+s1 = "Hello, "
+s2 = "Vetit "
+# print(s1 + s2)
+# print(s1 * 5)
+# length
+print(len(s1))
+# indexing
+# "Hello, world!" --> H is in position 0, blank space counts as a character.
+# Possible numbers for index are from -len() to len() - 1\
+print(s1[5])
+# slicing
+# s[2:5] = s[2] + s[3] + s[4] || Same as range
+print(s2[2:5])
