ce359e3c2458d997f13ea9882f90da1922f93145
Art38169 <00482@kvis.ac.th>
2024-09-19 20:05:47 +0700

    day9.py

---
+++ day9.py
@@ -0,0 +1,13 @@
+print("My project uses luck only.")
+# List
+# empty_list = [] OR list() || numbers = [1, 2, 3, 4]
+# my_list = [1, 0.2, True, "Vetit", None, [2, 3, 4], print(), input(), empty_list]
+# Slice list, get list.
+# ADD
+# l = [1, 2, 3],  l.append(4) --> l = [1, 2, 3, 4] || append is to put at last.
+# l.insert(2, True) --> l = [1, 2, True, 3]
+# l.extend([5, 6, 7, 8, 9, 10]) --> l = [1, 2, 3, 5, 6, 7, 8, 9, 10]
+# REMOVE
+# last = l.pop() --> l = [1, 2], last = 3 || Supports index -> l.pop(1) will remove 2
+# l.remove(2) --> l = [1, 3] || If not in the list, gives error
+# 
