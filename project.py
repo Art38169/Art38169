efe728fecf9dab1ef49d4e3900ea5970b26bb490
Art38169 <00482@kvis.ac.th>
2024-09-26 14:10:48 +0700

    turtle

--- project.py
+++ project.py
@@ -3,3 +3,4 @@ attack_dice = [0, 1, 2, 3, 4, 5, 6]
 moving_dice = [-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6]
 health = 40
 crit_chance = [0,1]
+print(random.choices(crit_chance, weights=[0.9, 0.1]))
